53345cd5815bb90ecf9455ab813ac26e9b8f4576
b.dan.18.09@gmail.com <b.dan.18.09@gmail.com>
2021-02-02 15:15:00 +0200

    Добавьте файлы проекта.

---
+++ prk/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace prk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить график 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "построить график 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(621, 110);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(618, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Масштаб";
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(671, 196);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(25, 23);
+            this.buttonRight.TabIndex = 7;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(652, 215);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(25, 23);
+            this.buttonDown.TabIndex = 8;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(631, 196);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(25, 23);
+            this.buttonLeft.TabIndex = 9;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonUp.Location = new System.Drawing.Point(652, 176);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(25, 27);
+            this.buttonUp.TabIndex = 6;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 421);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonLeft;
+    }
+}
+
---
+++ prk/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+namespace prk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool gr1;
+        int xcentr = 600 / 2;
+        int ycentr = 400 / 2;
+        double funct(double x)
+        {	
+            int scale = (int)(numericUpDown1.Value);
+            double size = 0.4 * scale;
+            if ((x < 2 * size) && (x > size)) return (Math.Pow(x / size, 1.0 / 2) * Math.Sin(Math.Pow(x / size, 2)) - 1.3) / (Math.Pow(x / size, 1.0 / 3) + Math.Exp(2 * x / size) + Math.Abs(Math.Cos(x / size))) * size;
+            else if (((x <= size) && (x > 0)) || ((x > -5 * size) && (x < 0))) return 1 / Math.Pow(x / size, 3) * size;
+            else return Math.Pow(x, 2) / size;
+        }
+        void Graph1(int scale)
+        {
+            Pen pen1 = new Pen(Brushes.Black, 1);
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.NoClip;
+            double size = 0.4 * scale;
+            g.DrawLine(pen1, new Point(0, ycentr), new Point(pictureBox1.Width, ycentr));
+            g.DrawLine(pen1, new Point(xcentr, 0), new Point(xcentr, pictureBox1.Height));
+            for (int i = -(int)(xcentr / size); i < 0; i++)// разбивка ох
+            {
+                for (int j = 0; j <= pictureBox1.Height; j += 5)
+                    g.DrawEllipse(pen1, (int)(xcentr + (size * i)), j, 1, 1);
+                g.DrawLine(pen1, new Point((int)(xcentr + (size * i)), (int)(ycentr - 0.04 * scale)), new Point((int)(xcentr + (size * i)), (int)(ycentr + 0.04 * scale)));
+                g.DrawString(i.ToString(), new Font("Calibri", 10), new SolidBrush(Color.Black), (float)(xcentr + (int)(size * i) - 5), (float)(ycentr + 6 / 100 * scale), drawFormat);
+            }
+            for (int i = 1; i <= (int)((pictureBox1.Width - xcentr) / size); i++)
+            {
+                for (int j = 0; j <= pictureBox1.Height; j += 5)
+                    g.DrawEllipse(pen1, (int)(xcentr + (size * i)), j, 1, 1);
+                g.DrawLine(pen1, new Point((int)(xcentr + (size * i)), (int)(ycentr - 0.04 * scale)), new Point((int)(xcentr + (size * i)), (int)(ycentr + 0.04 * scale)));
+                g.DrawString(i.ToString(), new Font("Calibri", 10), new SolidBrush(Color.Black), (float)(xcentr + (int)(size * i) - 5), (float)(ycentr + 6 / 100 * scale), drawFormat);
+            }
+            for (int j = -(int)(ycentr / size); j < 0; j++)// разбивка оy
+            {
+                for (int i = 0; i <= pictureBox1.Width; i += 5)
+                    g.DrawEllipse(pen1, i, (int)(ycentr + (size * j)), 1, 1);
+                g.DrawLine(pen1, new Point((int)(xcentr - 0.04 * scale), (int)(ycentr + (size * j))), new Point((int)(xcentr + 0.04 * scale), (int)(ycentr + (size * j))));
+                g.DrawString(j.ToString(), new Font("Calibri", 10), new SolidBrush(Color.Black), (float)(xcentr - 20 / 100 * scale), (float)(ycentr - (size * j) - 10), drawFormat);
+            }
+            for (int j = 1; j <= (int)(pictureBox1.Height - ycentr / size); j++)
+            {
+                for (int i = 0; i <= pictureBox1.Width; i += 5)
+                    g.DrawEllipse(pen1, i, (int)(ycentr + (size * j)), 1, 1);
+                g.DrawLine(pen1, new Point((int)(xcentr - 0.04 * scale), (int)(ycentr + (size * j))), new Point((int)(xcentr + 0.04 * scale), (int)(ycentr + (size * j))));
+                g.DrawString(j.ToString(), new Font("Calibri", 10), new SolidBrush(Color.Black), (float)(xcentr - 20 / 100 * scale), (float)(ycentr - (size * j) - 10), drawFormat);
+            }
+            g.DrawLine(pen1, new Point(pictureBox1.Width - 15, ycentr + 25), new Point(pictureBox1.Width - 5, ycentr + 5));//x 
+            g.DrawLine(pen1, new Point(pictureBox1.Width - 5, ycentr + 25), new Point(pictureBox1.Width - 15, ycentr + 5));
+            g.DrawLine(pen1, new Point(xcentr + 5, 5), new Point(xcentr + 10, 15));//y
+            g.DrawLine(pen1, new Point(xcentr + 10, 15), new Point(xcentr + 15, 5));
+            g.DrawLine(pen1, new Point(xcentr + 10, 25), new Point(xcentr + 10, 15));
+            g.DrawLine(pen1, new Point(xcentr, 0), new Point(xcentr - 5, 5));//стрелки
+            g.DrawLine(pen1, new Point(xcentr, 0), new Point(xcentr + 5, 5));
+            g.DrawLine(pen1, new Point(pictureBox1.Width, ycentr), new Point(pictureBox1.Width - 5, ycentr - 5));
+            g.DrawLine(pen1, new Point(pictureBox1.Width, ycentr), new Point(pictureBox1.Width - 5, ycentr + 5));
+            pen1.Color = Color.Red;
+            for(int i = -xcentr; i < pictureBox1.Width-xcentr; i++)
+                g.DrawEllipse(pen1, xcentr + i, (ycentr - (int)funct(i)), 1, 1);
+        }
+        void Graph2(int scale)
+        {
+            Pen pen1 = new Pen(Brushes.Black, 1);
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.NoClip;
+            double size = 0.4 * scale;
+            g.DrawLine(pen1, new Point(0, ycentr), new Point(pictureBox1.Width, ycentr));
+            g.DrawLine(pen1, new Point(xcentr, 0), new Point(xcentr, pictureBox1.Height));
+            for (int i = -(int)(xcentr / size); i < 0; i++)// разбивка ох
+            {
+                for (int j = 0; j <= pictureBox1.Height; j += 5)
+                    g.DrawEllipse(pen1, (int)(xcentr + (size * i)), j, 1, 1);
+                g.DrawLine(pen1, new Point((int)(xcentr + (size * i)), (int)(ycentr - 0.04 * scale)), new Point((int)(xcentr + (size * i)), (int)(ycentr + 0.04 * scale)));
+                g.DrawString(i.ToString(), new Font("Calibri", 10), new SolidBrush(Color.Black), (float)(xcentr + (int)(size * i) - 5), (float)(ycentr + 6 / 100 * scale), drawFormat);
+            }
+            for (int i = 1; i <= (int)((pictureBox1.Width - xcentr) / size); i++)
+            {
+                for (int j = 0; j <= pictureBox1.Height; j += 5)
+                    g.DrawEllipse(pen1, (int)(xcentr + (size * i)), j, 1, 1);
+                g.DrawLine(pen1, new Point((int)(xcentr + (size * i)), (int)(ycentr - 0.04 * scale)), new Point((int)(xcentr + (size * i)), (int)(ycentr + 0.04 * scale)));
+                g.DrawString(i.ToString(), new Font("Calibri", 10), new SolidBrush(Color.Black), (float)(xcentr + (int)(size * i) - 5), (float)(ycentr + 6 / 100 * scale), drawFormat);
+            }
+            for (int j = -(int)((pictureBox1.Height - ycentr) / size); j < 0; j++)// разбивка оy
+            {
+                for (int i = 0; i <= pictureBox1.Width; i += 5)
+                    g.DrawEllipse(pen1, i, (int)(ycentr - (size * j)), 1, 1);
+                g.DrawLine(pen1, new Point((int)(xcentr - 0.04 * scale), (int)(ycentr + (size * j))), new Point((int)(xcentr + 0.04 * scale), (int)(ycentr + (size * j))));
+                g.DrawString(j.ToString(), new Font("Calibri", 10), new SolidBrush(Color.Black), (float)(xcentr - 20 / 100 * scale), (float)(ycentr - (size * j) - 10), drawFormat);
+            }
+            for (int j = 1; j <= (int)(ycentr / size); j++)
+            {
+                for (int i = 0; i <= pictureBox1.Width; i += 5)
+                    g.DrawEllipse(pen1, i, (int)(ycentr - (size * j)), 1, 1);
+                g.DrawLine(pen1, new Point((int)(xcentr - 0.04 * scale), (int)(ycentr + (size * j))), new Point((int)(xcentr + 0.04 * scale), (int)(ycentr + (size * j))));
+                g.DrawString(j.ToString(), new Font("Calibri", 10), new SolidBrush(Color.Black), (float)(xcentr - 20 / 100 * scale), (float)(ycentr - (size * j) - 10), drawFormat);
+            }
+            g.DrawLine(pen1, new Point(pictureBox1.Width - 15, ycentr + 25), new Point(pictureBox1.Width - 5, ycentr + 5));//x 
+            g.DrawLine(pen1, new Point(pictureBox1.Width - 5, ycentr + 25), new Point(pictureBox1.Width - 15, ycentr + 5));
+            g.DrawLine(pen1, new Point(xcentr + 5, 5), new Point(xcentr + 10, 15));//y
+            g.DrawLine(pen1, new Point(xcentr + 10, 15), new Point(xcentr + 15, 5));
+            g.DrawLine(pen1, new Point(xcentr + 10, 25), new Point(xcentr + 10, 15));
+            g.DrawLine(pen1, new Point(xcentr, 0), new Point(xcentr - 5, 5));//стрелки
+            g.DrawLine(pen1, new Point(xcentr, 0), new Point(xcentr + 5, 5));
+            g.DrawLine(pen1, new Point(pictureBox1.Width, ycentr), new Point(pictureBox1.Width - 5, ycentr - 5));
+            g.DrawLine(pen1, new Point(pictureBox1.Width, ycentr), new Point(pictureBox1.Width - 5, ycentr + 5));
+            pen1.Color = Color.Green;
+            StreamReader sr = new StreamReader("input.txt");
+            {
+                string[] text; double x, y, x2, y2;
+                while (!sr.EndOfStream)
+                {
+                    text = sr.ReadLine().Split(' ');
+                    x = double.Parse(text[0], CultureInfo.InvariantCulture);
+                    y = double.Parse(text[1], CultureInfo.InvariantCulture);
+                    text = sr.ReadLine().Split(' ');
+                    x2 = double.Parse(text[0], CultureInfo.InvariantCulture);
+                    y2 = double.Parse(text[1], CultureInfo.InvariantCulture);
+                    g.DrawLine(pen1, new Point(xcentr + (int)(x*size), ycentr - (int)(y*size)), new Point(xcentr + (int)(x2*size), ycentr - (int)(y2*size)));
+                    //g.DrawEllipse(pen1, xcentr + (int)(x*size), ycentr - (int)(y*size), 1, 1);
+                }
+                sr.Close();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int scale = (int)(numericUpDown1.Value);
+            Graph1(scale);
+            gr1 = true;
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            int scale = (int)(numericUpDown1.Value);
+            if (gr1)
+            {
+                ycentr += (int)(0.4 * scale);
+                Graph1(scale);
+            }
+            else
+            {
+                ycentr += (int)(0.4 * scale);
+                Graph2(scale);
+            }
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            int scale = (int)(numericUpDown1.Value);
+            if (gr1)
+            {
+                ycentr -= (int)(0.4 * scale);
+                Graph1(scale);
+            }
+            else
+            {
+                ycentr -= (int)(0.4 * scale);
+                Graph2(scale);
+            }
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            int scale = (int)(numericUpDown1.Value);
+            if (gr1)
+            {
+                xcentr -= (int)(0.4 * scale);
+                Graph1(scale);
+            }
+            else
+            {
+                xcentr -= (int)(0.4 * scale);
+                Graph2(scale);
+            }
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            int scale = (int)(numericUpDown1.Value);
+            if (gr1)
+            {
+                xcentr += (int)(0.4 * scale);
+                Graph1(scale);
+            }
+            else
+            {
+                xcentr += (int)(0.4 * scale);
+                Graph2(scale);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int scale = (int)(numericUpDown1.Value);
+            Graph2(scale);
+            gr1 = false;
+        }
+    }
+}
